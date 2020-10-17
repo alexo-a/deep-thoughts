8eb98992a5ebecc2a7a204406bd253c46757b5b3
Alex Ostrowski <a.ostrowski.11@gmail.com>
2020-10-17 12:00:27 -0500

    set up full stack structure (concurrently)

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const db = require('./config/connection');
 //import middleware
 const { authMiddleware } = require('./utils/auth');
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
@@ -23,6 +25,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
