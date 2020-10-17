52030bb32fbff3793f161e4f2958160a9aab3004
Alex Ostrowski <a.ostrowski.11@gmail.com>
2020-10-17 15:58:06 -0500

    added friend functionality and thought comp.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,11 +3,13 @@ import { Redirect, useParams } from 'react-router-dom';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-import { useQuery } from '@apollo/react-hooks';
 import Auth from '../utils/auth';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
-
+    const [addFriend] = useMutation(ADD_FRIEND);
     const { username: userParam } = useParams();
 
     const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -32,6 +34,16 @@ const Profile = () => {
             </h4>
         );
     }
+
+    const handleClick = async () => {
+        try {
+            await addFriend({
+                variables: { id: user._id }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    };
     
     return (
         <div>
@@ -39,6 +51,12 @@ const Profile = () => {
                 <h2 className="bg-dark text-secondary p-3 display-inline-block">
                     Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
+
+                {userParam && (
+                    <button className="btn ml-auto" onClick={handleClick}>
+                        Add Friend
+                    </button>
+                )}
             </div>
 
             <div className="flex-row justify-space-between mb-3">
@@ -53,6 +71,7 @@ const Profile = () => {
                     />
                 </div>
             </div>
+            <div className="mb-3">{!userParam && <ThoughtForm />}</div>
         </div>
     );
 };
